5ebb007eabb5b599e95857fd09e38e1079783fd1
mebarrionuevo20-jpg <mebarrionuevo20@gmail.com>
2026-07-11 11:17:55 -0300

    Update consultas_basicas.sql

--- consultas_basicas.sql
+++ consultas_basicas.sql
@@ -2,7 +2,7 @@
 -- Autor: Eugenia Barrionuevo
 -- Fecha: 11/07/2026
 
--- Consulta 1: Exploración general de la tabla sales
+-- Consulta 1: Exploración general de la tabla sales. SELECT * sirve cuando queremos explorar una tabla por primera vez y conocer todas sus columnas y registros. No conviene usar SELECT * en reportes finales o consultas grandes, porque trae columnas innecesarias y puede hacer más lenta la consulta.
 -- SELECT * FROM sales;
 -- Consulta 2: Selección de columnas específicas para finanzas
 -- SELECT customer_id, product_id, total_amount FROM sales;
